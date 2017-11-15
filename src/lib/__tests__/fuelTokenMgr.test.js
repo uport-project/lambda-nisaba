55088d234f6206f4585429eab7d34a75488fbc9e
Andres Junge <ajunge@yaykuy.cl>
2017-11-15 00:34:26 -0300

    test stub

---
+++ src/lib/__tests__/fuelTokenMgr.test.js
@@ -0,0 +1,15 @@
+const FuelTokenMgr = require('../fuelTokenMgr');
+
+describe('FuelTokenMgr', () => {
+    
+    let sut;
+    
+    beforeAll(() => {
+        sut = new FuelTokenMgr();
+    });
+
+    test('empty constructor', () => {
+        expect(sut).not.toBeUndefined();
+    });
+
+});
