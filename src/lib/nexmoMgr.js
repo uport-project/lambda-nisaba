e7df03648b35df401e136ba01df90c124c65a389
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-03-09 17:38:46 -0300

    nexmo verification handler

---
+++ src/lib/nexmoMgr.js
@@ -0,0 +1,200 @@
+import Nexmo from "nexmo";
+import { decodeToken, TokenVerifier } from "jsontokens";
+import { Client } from 'pg'
+
+class NexmoMgr {
+  constructor(phone_from) {
+    this.pgUrl = null
+    this.client = null;
+    this.from = phone_from;
+  }
+
+  isSecretsSet() {
+    return (this.api_key !== null || this.api_secret !== null || this.pgUrl !== null);
+  }
+
+  setSecrets(secrets) {
+    this.api_key = secrets.NEXMO_API_KEY;
+    this.api_secret = secrets.NEXMO_SECRET;
+    this.client = new Nexmo({
+      apiKey: this.api_key,
+      apiSecret: this.api_secret
+    }, {
+      debug: false,
+      appendToUserAgent: "uPort/nisaba"
+    });
+  }
+
+  async start(deviceKey, phoneNumber) {
+    if (!deviceKey) throw "no deviceKey";
+    if (!phoneNumber) throw "no destination phoneNumber";
+    if (!this.client) throw "client is not initialized";
+
+    let params = {
+      number: phoneNumber,
+      brand: "uPort",
+      code_length: 6
+    };
+
+    this.client.verify.request(params, (err, resp) => {
+      if (err) {
+        console.log("error on nexmo.verify.request", err);
+        throw ("error calling nexmo.verify.request");
+      }
+
+      console.log("nexmo.status: ", resp.status);
+      if (resp.status == "0" || resp.status == "10") {
+        //00 > Success - The request was successfully accepted by Nexmo.
+        //10 > Concurrent verifications to the same number are not allowed.
+        this.createRequest(deviceKey, resp.request_id, resp.status);
+        return;
+      } else {
+        //Basically, an error
+        console.log("Nexmo error: ", resp.status, ". Phone: ", phoneNumber)
+        return ({
+          error: resp.status,
+          phone: phoneNumber
+        });
+      }
+    });
+  }
+
+  async next(deviceKey) {
+    return this.control(deviceKey, "trigger_next_event")
+  }
+
+  async cancel(deviceKey) {
+    return this.control(deviceKey, "cancel")
+  }
+
+  async control(deviceKey, command) {
+    if (!deviceKey) throw "no deviceKey";
+    if (!this.command) throw "no command";
+    if (!this.client) throw "client is not initialized";
+
+    let requestId;
+    requestId = this.getRequest(deviceKey)
+    if (!requestId) throw "request_id not found"
+
+    let params = {
+      request_id: requestId,
+      cmd: command
+    }
+
+    this.client.verify.control(params, (err, resp) => {
+      if (err) {
+        console.error("error calling nexmo.verify.control", command, err)
+        throw "error on nexmo.verify.control";
+      }
+
+      if (resp.status !== '0') {
+        console.log("nexmo.status:", resp.status, "error:", resp.error_text)
+        return ({
+          status: resp.status,
+          error: resp.error_text
+        });
+      } else {
+        return;
+      }
+    });
+  }
+
+  async check(deviceKey, code) {
+    if (!deviceKey) throw "no deviceKey";
+    if (!code) throw "no code";
+    if (!this.client) throw "client is not initialized";
+
+    let requestId;
+    requestId = this.getRequest(deviceKey)
+    if (!requestId) throw "request_id not found"
+
+    let params = {
+      request_id: requestId,
+      code: code
+    }
+
+    this.client.verify.check(params, (err, resp) => {
+      if (err) {
+        console.error("error calling nexmo.verify.check", err)
+        throw "error on nexmo.verify.check";
+      }
+
+      if (resp.status == '0') {
+        this.deleteRequest(deviceKey)
+        return;
+      } else {
+        return ({
+          status: resp.status,
+          error: resp.error_text
+        })
+      }
+    })
+  }
+
+  async createRequest(deviceKey, requestId, reqStatus) {
+    if (!this.pgUrl) throw ('no pgUrl set')
+    if (!this.deviceKey) throw ('no device key')
+    if (!this.requestId) throw ('no nexmo request id')
+    if (!this.reqStatus) throw ('no nexmo request status')
+
+    const client = new Client({
+      connectionString: this.pgUrl,
+    })
+
+    try {
+      await client.connect()
+      const res = await client.query(
+        "INSERT INTO nexmo_requests(device_key, request_id, request_status) VALUES ($1,$2,$3);",
+        [deviceKey, requestId, reqStatus]);
+      return;
+    } catch (e) {
+      throw (e);
+    } finally {
+      await client.end()
+    }
+  }
+
+  async getRequest(deviceKey) {
+    if (!this.pgUrl) throw ('no pgUrl set')
+    if (!this.deviceKey) throw ('no device key')
+
+    const client = new Client({
+      connectionString: this.pgUrl,
+    })
+
+    try {
+      await client.connect()
+      const res = await client.query(
+        "SELECT request_id FROM nexmo_requests WHERE device_key=$1;",
+        [deviceKey]);
+      return res.rows[0].request_id;
+    } catch (e) {
+      throw (e);
+    } finally {
+      await client.end()
+    }
+  }
+
+  async deleteRequest(deviceKey) {
+    if (!this.pgUrl) throw ('no pgUrl set')
+    if (!this.deviceKey) throw ('no devicekey')
+
+    const client = new Client({
+      connectionString: this.pgUrl,
+    })
+
+    try {
+      await client.connect()
+      const res = await client.query(
+        "DELETE FROM nexmo_requests WHERE device_key = $1;", [deviceKey]);
+      return;
+    } catch (e) {
+      throw (e);
+    } finally {
+      await client.end()
+    }
+  }
+
+}
+
+module.exports = NexmoMgr;
