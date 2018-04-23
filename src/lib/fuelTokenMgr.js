d4a7506a34055b06a499e83b7067608367442e5d
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-04-23 19:46:48 -0300

    use did-jwt when sign and verify payloads

--- src/lib/fuelTokenMgr.js
+++ src/lib/fuelTokenMgr.js
@@ -1,4 +1,5 @@
-import { TokenSigner, TokenVerifier, decodeToken } from "jsontokens";
+import { decodeToken } from "jsontokens";
+import { createJWT, verifyJWT, SimpleSigner } from "did-jwt";
 
 class FuelTokenMgr {
   constructor() {
@@ -13,24 +14,40 @@ class FuelTokenMgr {
   setSecrets(secrets) {
     this.signingKey = secrets.FUEL_TOKEN_PRIVATE_KEY;
     this.publicKey = secrets.FUEL_TOKEN_PUBLIC_KEY;
-    this.tokenSigner = new TokenSigner("ES256K", this.signingKey);
+    this.tokenSigner = SimpleSigner(this.signingKey);
   }
 
   async newToken(deviceKey) {
+    const signer = this.tokenSigner;
     let now = Math.floor(Date.now() / 1000);
-    let payload = {
-      iss: "api.uport.me/nisaba",
-      iat: now,
-      exp: now + 300,
-      sub: deviceKey,
-      aud: ["api.uport.me/nisaba", "api.uport.me/unnu", "api.uport.me/sensui"]
-    };
-    let signedJwt = this.tokenSigner.sign(payload);
-    return signedJwt;
+    return createJWT(
+      {
+        aud: [
+          "api.uport.me/nisaba",
+          "api.uport.me/unnu",
+          "api.uport.me/sensui"
+        ],
+        exp: now + 300,
+        sub: deviceKey,
+        iat: now
+      },
+      { issuer: "api.uport.me/nisaba", signer }
+    ).then(jwt => {
+      return jwt;
+    });
   }
 
   async verifyToken(fuelToken) {
-    return new TokenVerifier("ES256k", this.publicKey).verify(fuelToken);
+    let aud = [
+      "api.uport.me/nisaba",
+      "api.uport.me/unnu",
+      "api.uport.me/sensui"
+    ];
+    verifyJWT(fuelToken, {
+      audience: aud
+    }).then(({ payload, doc, did, signer, jwt }) => {
+      console.log(payload);
+    });
   }
 
   async decode(fuelToken) {
