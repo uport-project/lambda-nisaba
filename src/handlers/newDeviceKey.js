88705ff45c11145bf1cb2c0bb2c6c17522550a03
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-05-04 19:47:20 -0300

    logging to catch error

--- src/handlers/newDeviceKey.js
+++ src/handlers/newDeviceKey.js
@@ -68,10 +68,10 @@ class NewDeviceKeyHandler {
     }
     console.log(dRequestToken);
 
+    console.log("dRequestToken publicKey", dRequestToken.doc.publicKey);
     const pubKey = dRequestToken.doc.publicKey.find(
       pub => pub.type === "Secp256k1VerificationKey2018"
     );
-    console.log("pubkey", pubKey);
     const address =
       pubKey.ethereumAddress || toEthereumAddress(pubKey.publicKeyHex);
 
