835674a208ec34aab9d186bd6f2cf5c46a686114
Robert Greenfield IV <robertgreenfieldiv@Roberts-MacBook.local>
2018-06-17 20:49:28 -0700

    removed all instances of funcaptcha since it wont be used and the service is far to expensive to be part of the generic solution

--- src/api_handler.js
+++ src/api_handler.js
@@ -12,7 +12,6 @@ const AttestationMgr = require("./lib/attestationMgr");
 const PhoneVerificationMgr = require("./lib/phoneVerificationMgr");
 
 const RecaptchaHandler = require("./handlers/recaptcha");
-const FuncaptchaHandler = require("./handlers/funcaptcha");
 const NewDeviceKeyHandler = require("./handlers/newDeviceKey");
 const PhoneAttestationHandler = require("./handlers/phone_attestation");
 const StartVerificationHandler = require("./handlers/start_verification");
@@ -21,7 +20,6 @@ const CheckVerificationHandler = require("./handlers/check_verification");
 
 //instantiate manager services needed for methods
 let recaptchaMgr = new RecaptchaMgr(); //setting and verify the captcha token
-let funcaptchaMgr = new FuncaptchaMgr(); //interactive captcha service (JWT token generation)
 let authMgr = new AuthMgr(); //verifies authorization request to nisaba service
 let fuelTokenMgr = new FuelTokenMgr(); //develops new JWT tokens for new users
 let uPortMgr = new UPortMgr(); //uport specific JWT token verification
@@ -29,7 +27,6 @@ let attestationMgr = new AttestationMgr(); // Create attestation for the subscri
 let phoneVerificationMgr = new PhoneVerificationMgr(); //Verify phone number code sent via text
 
 let recaptchaHandler = new RecaptchaHandler(recaptchaMgr, fuelTokenMgr);
-let funcaptchaHandler = new FuncaptchaHandler(funcaptchaMgr, fuelTokenMgr);
 let newDeviceKeyHandler = new NewDeviceKeyHandler(
   authMgr,
   uPortMgr,
@@ -53,11 +50,6 @@ let checkVerificationHandler = new CheckVerificationHandler(
 //verifies recaptcha token and provides fuel token
 module.exports.recaptcha = (event, context, callback) => {
   postHandler(recaptchaHandler, event, context, callback);
-};
-
-//verifies funcaptcha token and provides fuel token
-module.exports.funcaptcha = (event, context, callback) => {
-  postHandler(funcaptchaHandler, event, context, callback);
 };
 
 //Get a new fuel token by sending a signed requestToken
@@ -88,7 +80,6 @@ module.exports.check_verification = (event, context, callback) => {
 const postHandler = (handler, event, context, callback) => {
   if (
     !recaptchaMgr.isSecretsSet() ||
-    !funcaptchaMgr.isSecretsSet() ||
     !authMgr.isSecretsSet() ||
     !fuelTokenMgr.isSecretsSet() ||
     !phoneVerificationMgr.isSecretsSet()
@@ -102,7 +93,6 @@ const postHandler = (handler, event, context, callback) => {
       .then(data => {
         const decrypted = String(data.Plaintext);
         recaptchaMgr.setSecrets(JSON.parse(decrypted));
-        funcaptchaMgr.setSecrets(JSON.parse(decrypted));
         authMgr.setSecrets(JSON.parse(decrypted));
         fuelTokenMgr.setSecrets(JSON.parse(decrypted));
         phoneVerificationMgr.setSecrets(JSON.parse(decrypted));
