4cbe3ef31a1d062efab71847f206e7468de758fd
Veronica Zheng <zhengzhixing@gmail.com>
2018-06-25 15:39:14 -0400

    use FULE_TOKEN_ADDRESS and SENSUI_ADDRESS for issuer and aud

--- src/lib/fuelTokenMgr.js
+++ src/lib/fuelTokenMgr.js
@@ -20,34 +20,34 @@ class FuelTokenMgr {
   constructor() {
     this.signingKey = null;
     this.publicKey = null;
+    this.address = null;
     this.tokenSigner = null;
   }
   isSecretsSet() {
-    return this.signingKey !== null && this.publicKey !== null;
+    return this.signingKey !== null && this.publicKey !== null && this.address != null;
   }
 
   setSecrets(secrets) {
     this.signingKey = secrets.FUEL_TOKEN_PRIVATE_KEY;
     this.publicKey = secrets.FUEL_TOKEN_PUBLIC_KEY;
+    this.address = secrets.FUEL_TOKEN_ADDRESS;
+    this.aud = secrets.SENSUI_ADDRESS ?
+      [secrets.FUEL_TOKEN_ADDRESS, secrets.SENSUI_ADDRESS] : secrets.FUEL_TOKEN_ADDRESS;
     this.tokenSigner = SimpleSigner(this.signingKey);
   }
 
   /* Uport specific - needs to be changed for other builds */
   async newToken(deviceKey) {
     const signer = this.tokenSigner;
-    let now = Math.floor(Date.now() / 1000);
+    const now = Math.floor(Date.now() / 1000);
     return createJWT(
       {
-        aud: [
-          "api.uport.me/nisaba",
-          "api.uport.me/unnu",
-          "api.uport.me/sensui"
-        ],
+        aud: this.aud,
         exp: now + 300,
         sub: deviceKey,
         iat: now
       },
-      { issuer: "api.uport.me/nisaba", signer }
+      { issuer: this.address, signer }
     ).then(jwt => {
       return jwt;
     });
@@ -55,13 +55,8 @@ class FuelTokenMgr {
 
   /* Uport specific - needs to be changed for other builds */
   async verifyToken(fuelToken) {
-    let aud = [
-      "api.uport.me/nisaba",
-      "api.uport.me/unnu",
-      "api.uport.me/sensui"
-    ];
     verifyJWT(fuelToken, {
-      audience: aud
+      audience: this.aud
     }).then(({ payload, doc, did, signer, jwt }) => {
       console.log(payload);
     });
